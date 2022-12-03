45ae3f35b7cd72cb4a5a670ee1cb2d034ff15ae8
Mathieu Bour <mathieu.tin.bour@gmail.com>
2022-12-03 21:07:59 +0100

    feat(faq): added Chinese language

---
+++ src/constants/faq.en.tsx
@@ -0,0 +1,175 @@
+import { Stack, Text } from '@chakra-ui/react';
+
+export const titleEN = {
+  title: <>Frequently assked questions</>,
+  question: <>What is a non-fungible token (NFT)?</>,
+  answer: (
+    <>
+      NFT stands for &lsquo;Non-fungible token&rsquo;, which means that it&apos;s a unique digital item with
+      blockchain-managed ownership that users can buy, own, and trade. Some NFT&apos;s fundamental function is to be
+      digital art.
+      <br /> But they can also offer additional benefits like exclusive access to websites, event tickets, and ownership
+      records. NFTs can be used to represent easily-reproducible items such as photos, videos, audio, and other types of
+      digital files as unique items. NFTs use blockchain technology to establish a verified and public proof of
+      ownership. Stay safe, always do your own research.
+    </>
+  ),
+};
+
+export const questionsEN = [
+  {
+    question: <>What is The Assets Club?</>,
+    answer: (
+      <>
+        The Assets Club are unique Non-fungible ERC-721-A tokens created on Ethereum blockchain. Assets are an early
+        access p-ASS.{' '}
+      </>
+    ),
+  },
+  {
+    question: <>Is this a «profile picture project»?</>,
+    answer: (
+      <>
+        It was not designed to be. Please be careful when using it as profile picture on social networks like twitter,
+        the algorithm does not like overly naked assets.
+      </>
+    ),
+  },
+  {
+    question: <>How many Assets are there going to be?</>,
+    answer: <>There will be 5’777 unique and randomly generated assets. </>,
+  },
+  {
+    question: <>Do each Asset looks different?</>,
+    answer: <>All of them are unique. We have more than 25 different asset shapes!</>,
+  },
+  {
+    question: <>What is generative art?</>,
+    answer: (
+      <>
+        Generative art refers to any art practice where the artist creates a process, such as a set of natural language
+        rules, a computer program, a machine, or other procedural invention, which is then set into motion with some
+        degree of autonomy contributing to or resulting in a completed work of art.
+      </>
+    ),
+  },
+  {
+    question: <>What is the price of an Asset?</>,
+    answer: (
+      <Stack gap={1}>
+        <Text>Private sale: 1 Free NFT per wallet and 1 optional NFT for paid. Price TBA. (Less than 100$).</Text>
+        <Text>Public sale: Price TBA. (If there is a public sale).</Text>
+        <Text>
+          Please keep in mind that additional cost will occur because of transaction fees on the Ethereum blockchain.
+        </Text>
+      </Stack>
+    ),
+  },
+  {
+    question: <>How will The Assets Club funds be used?</>,
+    answer: (
+      <>
+        45% of the primary sale + 100% of the 5.5% secondary sales royalties contributed towards building out Inspirata.
+        55% of the primary sale will be used to pay the team.
+      </>
+    ),
+  },
+  {
+    question: <>How can I get on the Asset List?</>,
+    answer: (
+      <>
+        We frequently offer spots to our community and genuine people on Twitter/Discord. There will be raffles and it
+        will also be possible to apply through a form.
+      </>
+    ),
+  },
+  {
+    question: <>Do you have OG role?</>,
+    answer: (
+      <>
+        We have a limited number of « Original Asset » roles for those who wish to be more closely involved in this
+        journey. If you wish to apply, please send an application containing your presentation socials and references
+        to: contact@theassets.club
+      </>
+    ),
+  },
+  {
+    question: <>What are the benefits of owning an Asset NFT?</>,
+    answer: (
+      <>
+        We really want to recognize our early supporters and community members. Owning Assets means you&apos;re one of
+        them. You will have priority access to our future project: Inspirata. Holding multiple Assets NFTs entitles you
+        to something that has not yet been disclosed.
+      </>
+    ),
+  },
+  {
+    question: <>When can I mint Assets?</>,
+    answer: <>To be announced.</>,
+  },
+  {
+    question: <>Which wallets are supported for minting?</>,
+    answer: <>You will be able to mint with any wallet supported by Wallet Connect, including MetaMask.</>,
+  },
+  {
+    question: <>Will the mint list spots be over-allocated?</>,
+    answer: <>Yes, slightly over-allocated.</>,
+  },
+  {
+    question: <>When reveal?</>,
+    answer: <>TBA.</>,
+  },
+  {
+    question: <>Smart contract released?</>,
+    answer: <>Written, not public yet.</>,
+  },
+  {
+    question: <>Token $ASS?</>,
+    answer: (
+      <>
+        Not planned, yet plausible. For any tokenomics, we will publish a white-paper first and If there is one, we’ll
+        make sure any token we come out with would adhere to regulations and be sustainable in our ecosystem.
+      </>
+    ),
+  },
+  {
+    question: <>Do you have a DAO?</>,
+    answer: <>No, we do not have a DAO, and it is not currently on our agenda.</>,
+  },
+  {
+    question: <>Do you have lands?</>,
+    answer: <>No, we do not offer lands, and land is not on our agenda.</>,
+  },
+  {
+    question: <>What intellectual property rights do Asset holders receive?</>,
+    answer: (
+      <>
+        Do whatever you want with your assets, you have every right to it. Full commercial art rights for the Asset NFT
+        token you own.
+      </>
+    ),
+  },
+  {
+    question: <>Laws, lawyers?</>,
+    answer: (
+      <>
+        We’ll be closely working with lawyers on our legal, regulatory and tax strategy for our holders benefits. Laws
+        around the world are catching up to the web3 innovation.
+      </>
+    ),
+  },
+  {
+    question: <>What is the value of this NFT?</>,
+    answer: <>This collection is first an artistic creation. The value of an Asset is what someone will pay for it.</>,
+  },
+  {
+    question: <>Last butt not least.</>,
+    answer: (
+      <>
+        Remember we will never do &ldquo;Stealth Mints&rdquo; or &ldquo;Surprise Drops&rdquo;. We will always announce
+        events weeks ahead and only through official channels. The team, mods and bots will never DM you and if you have
+        been contacted, they are 100% impersonators/imposters
+      </>
+    ),
+  },
+];
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,5 +1,7 @@
 import { NextPage } from 'next';
 import Image from 'next/future/image';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
 import {
   Accordion,
   AccordionButton,
@@ -8,8 +10,9 @@ import {
   AccordionPanel,
   Box,
   Container,
+  Flex,
   Heading,
-  Stack,
+  Link,
   Text,
 } from '@chakra-ui/react';
 import faq from '../../public/faq/faq.png';
@@ -18,166 +21,26 @@ import leafRight from '../../public/faq/leaf-right.png';
 import leafTop from '../../public/faq/leaf-top.png';
 import HeadingMF from '../components/HeadingMF';
 import Nav from '../components/Nav';
+import { questionsEN, titleEN } from '../constants/faq.en';
+import { questionsZH, titleZH } from '../constants/faq.zh';
 
-const questions = [
-  {
-    question: <>What is The Assets Club?</>,
-    answer: (
-      <>
-        The Assets Club are unique Non-fungible ERC-721-A tokens created on Ethereum blockchain. Assets are an early
-        access p-ASS.{' '}
-      </>
-    ),
+const languages = {
+  en: {
+    title: titleEN,
+    questions: questionsEN,
   },
-  {
-    question: <>Is this a «profile picture project»?</>,
-    answer: (
-      <>
-        It was not designed to be. Please be careful when using it as profile picture on social networks like twitter,
-        the algorithm does not like overly naked assets.
-      </>
-    ),
+  zh: {
+    title: titleZH,
+    questions: questionsZH,
   },
-  {
-    question: <>How many Assets are there going to be?</>,
-    answer: <>There will be 5’777 unique and randomly generated assets. </>,
-  },
-  {
-    question: <>Do each Asset looks different?</>,
-    answer: <>All of them are unique. We have more than 25 different asset shapes!</>,
-  },
-  {
-    question: <>What is generative art?</>,
-    answer: (
-      <>
-        Generative art refers to any art practice where the artist creates a process, such as a set of natural language
-        rules, a computer program, a machine, or other procedural invention, which is then set into motion with some
-        degree of autonomy contributing to or resulting in a completed work of art.
-      </>
-    ),
-  },
-  {
-    question: <>What is the price of an Asset?</>,
-    answer: (
-      <Stack gap={1}>
-        <Text>Private sale: 1 Free NFT per wallet and 1 optional NFT for paid. Price TBA. (Less than 100$).</Text>
-        <Text>Public sale: Price TBA. (If there is a public sale).</Text>
-        <Text>
-          Please keep in mind that additional cost will occur because of transaction fees on the Ethereum blockchain.
-        </Text>
-      </Stack>
-    ),
-  },
-  {
-    question: <>How will The Assets Club funds be used?</>,
-    answer: (
-      <>
-        45% of the primary sale + 100% of the 5.5% secondary sales royalties contributed towards building out Inspirata.
-        55% of the primary sale will be used to pay the team.
-      </>
-    ),
-  },
-  {
-    question: <>How can I get on the Asset List?</>,
-    answer: (
-      <>
-        We frequently offer spots to our community and genuine people on Twitter/Discord. There will be raffles and it
-        will also be possible to apply through a form.
-      </>
-    ),
-  },
-  {
-    question: <>Do you have OG role?</>,
-    answer: (
-      <>
-        We have a limited number of « Original Asset » roles for those who wish to be more closely involved in this
-        journey. If you wish to apply, please send an application containing your presentation socials and references
-        to: contact@theassets.club
-      </>
-    ),
-  },
-  {
-    question: <>What are the benefits of owning an Asset NFT?</>,
-    answer: (
-      <>
-        We really want to recognize our early supporters and community members. Owning Assets means you&apos;re one of
-        them. You will have priority access to our future project: Inspirata. Holding multiple Assets NFTs entitles you
-        to something that has not yet been disclosed.
-      </>
-    ),
-  },
-  {
-    question: <>When can I mint Assets?</>,
-    answer: <>To be announced.</>,
-  },
-  {
-    question: <>Which wallets are supported for minting?</>,
-    answer: <>You will be able to mint with any wallet supported by Wallet Connect, including MetaMask.</>,
-  },
-  {
-    question: <>Will the mint list spots be over-allocated?</>,
-    answer: <>Yes, slightly over-allocated.</>,
-  },
-  {
-    question: <>When reveal?</>,
-    answer: <>TBA.</>,
-  },
-  {
-    question: <>Smart contract released?</>,
-    answer: <>Written, not public yet.</>,
-  },
-  {
-    question: <>Token $ASS?</>,
-    answer: (
-      <>
-        Not planned, yet plausible. For any tokenomics, we will publish a white-paper first and If there is one, we’ll
-        make sure any token we come out with would adhere to regulations and be sustainable in our ecosystem.
-      </>
-    ),
-  },
-  {
-    question: <>Do you have a DAO?</>,
-    answer: <>No, we do not have a DAO, and it is not currently on our agenda.</>,
-  },
-  {
-    question: <>Do you have lands?</>,
-    answer: <>No, we do not offer lands, and land is not on our agenda.</>,
-  },
-  {
-    question: <>What intellectual property rights do Asset holders receive?</>,
-    answer: (
-      <>
-        Do whatever you want with your assets, you have every right to it. Full commercial art rights for the Asset NFT
-        token you own.
-      </>
-    ),
-  },
-  {
-    question: <>Laws, lawyers?</>,
-    answer: (
-      <>
-        We’ll be closely working with lawyers on our legal, regulatory and tax strategy for our holders benefits. Laws
-        around the world are catching up to the web3 innovation.
-      </>
-    ),
-  },
-  {
-    question: <>What is the value of this NFT?</>,
-    answer: <>This collection is first an artistic creation. The value of an Asset is what someone will pay for it.</>,
-  },
-  {
-    question: <>Last butt not least.</>,
-    answer: (
-      <>
-        Remember we will never do &ldquo;Stealth Mints&rdquo; or &ldquo;Surprise Drops&rdquo;. We will always announce
-        events weeks ahead and only through official channels. The team, mods and bots will never DM you and if you have
-        been contacted, they are 100% impersonators/imposters
-      </>
-    ),
-  },
-];
+};
 
 const FAQPage: NextPage = () => {
+  const router = useRouter();
+  const lang = router.query.lang === 'zh' ? 'zh' : 'en';
+  const title = languages[lang].title;
+  const questions = languages[lang].questions;
+
   return (
     <Box position="relative" color="white" bgColor="#1c201f" py={32}>
       <Nav position="fixed" top={0} left={0} right={0} zIndex={100} />
@@ -223,6 +86,20 @@ const FAQPage: NextPage = () => {
           <Image src={faq} alt="FAQ text logo" />
         </Box>
 
+        <Flex gap={4} textAlign="center" justifyContent="center" mx="auto">
+          <NextLink href="/faq" passHref>
+            <Link as={Text} textDecoration="underline" cursor="pointer">
+              English
+            </Link>
+          </NextLink>{' '}
+          |{' '}
+          <NextLink href="/faq?lang=zh" passHref>
+            <Link as={Text} textDecoration="underline" cursor="pointer">
+              中文
+            </Link>
+          </NextLink>
+        </Flex>
+
         <Container
           maxW={{ base: 'calc(100% - 32px)', md: '100ch' }}
           mx={{ base: 4, md: 'auto' }}
@@ -234,21 +111,13 @@ const FAQPage: NextPage = () => {
           borderRadius={8}
         >
           <HeadingMF as="h1" fontSize="6xl" mb={6}>
-            Frequently assked questions
+            {title.title}
           </HeadingMF>
           <Heading as="h2" fontSize="2xl" mb={6}>
-            What is a non-fungible token (NFT)?
+            {title.question}
           </Heading>
 
-          <Text lineHeight={1.8}>
-            NFT stands for &lsquo;Non-fungible token&rsquo;, which means that it&apos;s a unique digital item with
-            blockchain-managed ownership that users can buy, own, and trade. Some NFT&apos;s fundamental function is to
-            be digital art.
-            <br /> But they can also offer additional benefits like exclusive access to websites, event tickets, and
-            ownership records. NFTs can be used to represent easily-reproducible items such as photos, videos, audio,
-            and other types of digital files as unique items. NFTs use blockchain technology to establish a verified and
-            public proof of ownership. Stay safe, always do your own research.
-          </Text>
+          <Text lineHeight={1.8}>{title.answer}</Text>
         </Container>
 
         <Container maxW="80ch">
